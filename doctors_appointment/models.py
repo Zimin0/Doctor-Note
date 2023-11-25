b83d92bccafb5cbf46a40b279171c7e9f4346eda
Zimin0 <you@example.com>
2023-11-25 23:35:50 +0300

    convert_date was modified and convert_time was added

--- doctors_appointment/models.py
+++ doctors_appointment/models.py
@@ -6,12 +6,28 @@ class Appointment(models.Model):
     patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments')
     doctor = models.CharField(verbose_name="Врач", max_length=30, blank=False)
     date = models.DateField(verbose_name="Дата", max_length=10, help_text="Формат: XX.XX.XXXX", blank=False)
-    time = models.CharField(verbose_name="Время", max_length=5, help_text="Формат: XX:XX", blank=False)
+    time = models.TimeField(verbose_name="Время", max_length=5, help_text="Формат: XX:XX", blank=False)
     address = models.CharField(verbose_name="Адрес", max_length=100, blank=False)
     office_number = models.CharField(verbose_name="Кабинет", max_length=6, blank=False)
     health_troubles = models.TextField(verbose_name="Жалобы", blank=True)
+    archived = models.BooleanField(verbose_name="Архивировано?", default=False)
 
-    def convert_datetime(self, date_str:str):
-        """ Converts from "21.03.1980" to datetime.date object """
-        day, month, year = list(map(int, date_str.split('.'))) 
-        return datetime.date(year, month, day)
+    def convert_date(self, date_str:str) -> dict:
+        """ Converts data from "21.03.1980" to datetime.date object """
+        result = {'date':None, 'status':'OK'}
+        try:    
+            day, month, year = list(map(int, date_str.split('.'))) 
+            result['date'] = datetime.date(year, month, day)
+        except:
+            result['status'] = 'ERROR: invalid format of data.'
+        return result  
+    
+    def convert_time(self, time_str:str) -> dict:
+        """ Converts time from "12:45" to datetime.time object """
+        result = {'time':None, 'status':'OK'}
+        try:    
+            hour, minutes = list(map(int, time_str.split(':'))) 
+            result['time'] = datetime.time(hour, minutes, 00)
+        except:
+            result['status'] = 'ERROR: invalid format of time.'
+        return result  
--- doctors_appointment/views.py
+++ doctors_appointment/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+from doctors_appointment.models import Appointment
+
+@login_required
 def display_doctors(request):
+    user = request.user
+    appointments = Appointment.objects.filter() 
     return render(request, 'doctors_appointment/doctors.html', {})
 
+@login_required
 def add_doctors_appointment(request):
     return render(request, 'doctors_appointment/add.html', {})
