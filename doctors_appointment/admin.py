89b84e8b40284a9f96596ba4cb25d4f32d07ba57
Zimin0 <you@example.com>
2023-11-25 22:57:50 +0300

    Appointment models has been added and registered in admin panel

--- doctors_appointment/admin.py
+++ doctors_appointment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from doctors_appointment.models import Appointment
 
-# Register your models here.
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'doctor', 'date', 'time', 'address', 'office_number', 'health_troubles')
+    list_filter = ('patient', 'doctor', 'date', 'time')
+    search_fields = ('patient', 'doctor', 'date', 'time')
+
+admin.site.register(Appointment, AppointmentAdmin)
--- doctors_appointment/models.py
+++ doctors_appointment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Appointment(models.Model):
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments')
+    doctor = models.CharField(verbose_name="Врач", max_length=30, blank=False)
+    date = models.CharField(verbose_name="Дата", max_length=10, help_text="Формат: XX.XX.XXXX", blank=False)
+    time = models.CharField(verbose_name="Время", max_length=5, help_text="Формат: XX.XX", blank=False)
+    address = models.CharField(verbose_name="Адрес", max_length=100, blank=False)
+    office_number = models.CharField(verbose_name="Кабинет", max_length=6, blank=False)
+    health_troubles = models.TextField(verbose_name="Жалобы", blank=True)
+
