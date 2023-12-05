82b837c415baf844970e49058a8305d7d476660b
Zimin0 <you@example.com>
2023-12-06 01:10:54 +0300

    Top menu is working condition now

--- welcome/urls.py
+++ welcome/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from welcome.views import welcome
+from welcome.views import welcome, page_in_progress
 
 app_name = 'welcome'
 
 urlpatterns = [
-    path('', welcome, name='welcome'),
+    path('welcome/', welcome, name='welcome'),
+    path('in-progress/', page_in_progress, name='page_in_progress')
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def welcome(request):
     return render(request, 'welcome/welcome.html', {})
+
+def page_in_progress(request):
+    return render(request, 'welcome/inWork.html', {})
