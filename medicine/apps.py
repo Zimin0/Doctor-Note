ea8dd01f29e76e1c4c2d77238a0cee483532983a
Zimin0 <you@example.com>
2023-12-11 18:43:19 +0300

    Fixed some bugs

--- medicine/apps.py
+++ medicine/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MedicineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'medicine'
+    verbose_name = 'Выписанные лекарства'
--- medicine/models.py
+++ medicine/models.py
@@ -13,7 +13,7 @@ class Medicine(models.Model):
 
     name = models.CharField(verbose_name='Название', max_length=15)
     end_date = models.DateField(verbose_name='Последний день приема')
-    amount_per_day = models.CharField(verbose_name='Кол-во раз в день', max_length=20, help_text='Может содержать как число, так и описание: 2 таблетки в 3 дня' )
+    amount_per_day = models.CharField(verbose_name='Кол-во раз в день', max_length=40, help_text='Может содержать как число, так и описание: 2 таблетки в 3 дня' )
     dosage = models.CharField(verbose_name='Дозировка за один прием', max_length=30, help_text='Например: 3 капсулы')
     comments = models.TextField(verbose_name='Комментарии')
     is_ended = models.BooleanField(verbose_name='Прием лекарства закончился?', default=False)
@@ -31,7 +31,7 @@ class Medicine(models.Model):
     def is_medicine_over(self) -> bool:
         """ Проверяет, закончился ли прием лекарства. """
         current_timezone = timezone.get_current_timezone()
-        current_datetime = timezone.now()
-        medicine_datetime = datetime.datetime.combine(self.date, self.time)
-        medicine_datetime = medicine_datetime.replace(tzinfo=current_timezone)
-        return medicine_datetime < current_datetime
+        current_date = timezone.now()
+        medicine_date = self.end_date
+        medicine_datetime = datetime.datetime.combine(medicine_date, datetime.time(0, 0), tzinfo=current_timezone)
+        return medicine_datetime < current_date
