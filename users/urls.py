525b0a736ed07d6b337113c981e6946cc526f2d9
Zimin0 <you@example.com>
2023-12-13 15:21:13 +0300

    Field type was fixed. Logout url was added

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
-    
 ]
