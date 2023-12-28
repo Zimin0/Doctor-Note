91191fcbbbecf94d10557c70b4f19f3635017838
Zimin0 <you@example.com>
2023-12-28 23:56:48 +0300

    Add tests to doctors

--- doctors_appointment/tests.py
+++ doctors_appointment/tests.py
@@ -1,4 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
+from django.contrib.auth.models import User
 
-# Create your tests here. 
-# convert_date, convert_time
+# is_report_was_added, is_appointment_over
+
+class DoctorsAvailabilityTest(TestCase):
+    """ Проверяет, возможно ли зарегестрированному пользователю получить доступ к страницам сайта. """
+    def setUp(self):
+        # self.client = Client() # Можно не создавать 
+        self.user = User.objects.create_user('admin', 'admin@gmail.com', 'admin')
+        self.client.login(username='admin', password='admin')
+
+    def test_valid_status_code(self):
+        response = self.client.get('/doctors/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_needed_template_was_used(self):
+        response = self.client.get('/doctors/')
+        self.assertEqual(response.templates[0].name, 'doctors_appointment/doctors.html')
+
+    def tearDown(self):
+        ...
