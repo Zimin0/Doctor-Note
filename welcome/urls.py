f2fff0be263f693a218b18888dbf4dd2d3ba502a
Zimin0 <you@example.com>
2024-01-01 00:55:05 +0300

    Add redirect-to-page path into welcome app.

--- welcome/urls.py
+++ welcome/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from welcome.views import welcome, page_in_progress
+from welcome.views import welcome, page_in_progress, redirect_page_with_reason
 
 app_name = 'welcome'
 
 urlpatterns = [
     path('welcome/', welcome, name='welcome'),
-    path('in-progress/', page_in_progress, name='page_in_progress')
+    path('in-progress/', page_in_progress, name='page_in_progress'),
+    path('redirect-to/to=<str:to_url>', redirect_page_with_reason, name='redirect_page_with_reason')
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -5,3 +5,14 @@ def welcome(request):
 
 def page_in_progress(request):
     return render(request, 'welcome/inWork.html', {})
+
+def redirect_page_with_reason(request, to_url:str):
+    """ 
+    Перенаправление на заданную страницу с сообщением.
+    * to_url_name - строка формата "" welcome:page_in_progress 
+    """
+    context = {
+        'message': 'Успшено!',
+        'redirect_url' : to_url
+        }
+    return render(request, 'welcome/redirect.html', context)
