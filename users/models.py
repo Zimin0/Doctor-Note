06cc53ff0c158938b5ad97ca9b04fe5716fd7d11
Zimin0 <you@example.com>
2024-01-17 10:33:01 +0300

    Add users profiles. Name button leads to profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    need_to_send_notifics_on_mail = models.BooleanField(default=False)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from users.forms import UserRegistrationForm
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+from welcome.decorators import page_in_progress, log_veriables
+from users.forms import UserChangeInfoForm
 
 def register(request):
     if request.method == "POST":
@@ -16,3 +20,19 @@ def register(request):
     else:
         form = UserRegistrationForm()
         return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'GET':
+        form = UserChangeInfoForm(user=request.user)
+        return render(request, 'users/profile.html', {'form': form})
+    elif request.method == 'POST':
+        form = UserChangeInfoForm(request.POST, user=request.user)
+        if form.is_valid():
+            user = form.save()
+            profile = user.profile
+            profile.need_to_send_notifics_on_mail = form.cleaned_data.get('need_to_send_notifics_on_mail')
+            profile.save()
+            return render(request, 'users/profile.html', {'form': form})
+        else:
+            return render(request, 'users/profile.html', {'form': form, 'errors': form.errors})
