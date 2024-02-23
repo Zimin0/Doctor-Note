2bef6ea2df427802319570af24231deddc27eab3
Zimin0 <you@example.com>
2024-02-23 17:27:12 +0300

    Add new medicine template

--- medicine/admin.py
+++ medicine/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from medicine.models import Medicine
 
 class MedicineAdmin(admin.ModelAdmin):
-    list_display = ['title', 'dosage', 'end_date', 'amount_per_day', 'is_ended']
+    list_display = ['title', 'patient', 'dosage', 'end_date', 'amount_per_day', 'is_ended']
     
 
 admin.site.register(Medicine, MedicineAdmin)
