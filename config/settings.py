118036a4f87aa03f12584dfadad8a43f5f8071f9
Zimin0 <you@example.com>
2024-02-25 21:27:08 +0300

    Add translation to all pages

--- config/settings.py
+++ config/settings.py
@@ -55,8 +55,8 @@ REST_FRAMEWORK = {
 ## Translation ##
 LANGUAGE_CODE = 'ru' # по-умолчанию
 LANGUAGES = (
-    ('ru', _('Russian')),
-    ('en', _('English')),
+    ('ru', 'Russian'),
+    ('en', 'English'),
 )
 USE_I18N = True
 USE_L10N = True
@@ -149,7 +149,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
